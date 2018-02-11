43ec3e2b3dcc3e9d73dd8b39afc243ee89cc5bdc
Chirvasa Tudor <tudor.chirvasa@lisal.ro>
2018-02-11 16:12:15 +0200

    #269 Login lockout messages

--- login/index.php
+++ login/index.php
@@ -353,7 +353,7 @@ if (isloggedin() and !isguestuser()) {
     echo $OUTPUT->box_end();
 } else {
     $loginform = new \core_auth\output\login($authsequence, $frm->username);
-    if(!empty($CFG->lockoutduration)){
+    if($errorcode!==4 && !empty($CFG->lockoutduration)){
       $errormsg .= ' ' . get_string("lockoutloginwarning");
     }
     $loginform->set_error($errormsg);
